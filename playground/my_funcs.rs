c9c4730483a326cd0831731b9b5339f1cabcb324
oTTeuM <123246079+oTTeuMsTudio@users.noreply.github.com>
2024-11-17 19:11:58 +0100

    Update my_funcs.rs

--- playground/my_funcs.rs
+++ playground/my_funcs.rs
@@ -1,3 +1,16 @@
-pub fn add_five(num: u32) -> u32 {
+ pub fn add_five(num: u32) -> u32 {
     num + 5
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn adds_five_test() {
+        let x = 100;
+        let y = add_five(x);
+        println!("x and y is from test: {x} {y}");
+        assert_eq!(y, 105);
+    }
 }
